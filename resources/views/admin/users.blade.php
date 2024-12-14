c85a314a1e2062f36169dcf009932f499b2325d5
Peerawit Chanta <158536939+Dangpeerawit24@users.noreply.github.com>
2024-12-14 10:31:43 +0700

    first commit

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -87,6 +87,9 @@
                         <select name="type" id="type"
                             class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring focus:ring-blue-200">
                             <option value="">เลือกสิทธิ์การใช้งาน</option>
+                            <option value="1">
+                                แอดมิน
+                            </option>
                             <option value="2">
                                 ผู้จัดการ
                             </option>
@@ -151,6 +154,9 @@
                         <select name="type" id="type2" 
                             class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring focus:ring-blue-200">
                             <option value="">เลือกสิทธิ์การใช้งาน</option>
+                            <option value="1">
+                                แอดมิน
+                            </option>
                             <option value="2">
                                 ผู้จัดการ
                             </option>
