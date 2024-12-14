80a3ec0116f48b7ec17be09c60fab7feda7d39c8
Peerawit Chanta <158536939+Dangpeerawit24@users.noreply.github.com>
2024-12-14 10:06:43 +0700

    first commit

--- resources/views/admin/campaigns.blade.php
+++ resources/views/admin/campaigns.blade.php
@@ -467,7 +467,7 @@
             const startIndex = (currentPage - 1) * rowsPerPage;
             const endIndex = startIndex + rowsPerPage;
             const currentData = filteredData.slice(startIndex, endIndex);
-            const baseUrl = "{{ asset('img/campaign/') }}";
+            const baseUrl = "{{ asset('img/evidence/') }}";
 
             tableBody.innerHTML = '';
             currentData.forEach((Results, index) => {
