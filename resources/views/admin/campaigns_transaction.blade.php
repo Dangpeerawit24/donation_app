1e4e2eb7205b50891178c4e20fad0b604817ef59
Peerawit Chanta <158536939+Dangpeerawit24@users.noreply.github.com>
2024-12-18 23:29:49 +0700

    first commit

--- resources/views/admin/campaigns_transaction.blade.php
+++ resources/views/admin/campaigns_transaction.blade.php
@@ -365,7 +365,7 @@
 
         setInterval(() => {
             fetchData();
-        }, 30000);
+        }, 20000);
 
         document.getElementById('export-excel').addEventListener('click', () => {
             const table = document.querySelector('table');
