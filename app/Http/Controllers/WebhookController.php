46cb46a9d6875cbc509311af97e73dae60929e15
Peerawit Chanta <158536939+Dangpeerawit24@users.noreply.github.com>
2024-12-18 21:07:34 +0700

    first commit

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -46,7 +46,7 @@ class WebhookController extends Controller
 
                         if ($profile) {
                             // เก็บข้อมูลในฐานข้อมูล
-                            LineUser::updateOrCreate(
+                            LineUser::create(
                                 ['user_id' => $userId],
                                 [
                                     'display_name' => $profile['displayName'],
