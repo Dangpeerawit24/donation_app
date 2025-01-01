612f978a5d151a3e32c7eb80d25c197ee598ab92
Peerawit Chanta <158536939+Dangpeerawit24@users.noreply.github.com>
2025-01-01 19:42:01 +0700

    first commit

--- app/Http/Controllers/CampaignTransactionController.php
+++ app/Http/Controllers/CampaignTransactionController.php
@@ -209,7 +209,7 @@ class CampaignTransactionController extends Controller
             'value' => 'required|integer',
             'transactionID' => 'required',
             'details' => 'required',
-            'wish' => 'string',
+            'wish',
             'lineName' => 'required',
             'form' => 'required',
             'status' => 'required',
