d013cd9dbca18d96a27a694e7633d856089d5e6f
Peerawit Chanta <158536939+Dangpeerawit24@users.noreply.github.com>
2025-01-15 01:36:49 +0700

    first commit

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -93,7 +93,7 @@
 
 
         <!-- ใส่ div เปล่าสำหรับแสดงข้อมูล -->
-        <div id="modal-body" class="my-4 text-xl bg-gray-100 overflow-y-auto max-h-96">
+        <div id="modal-body" class="my-4 text-xl bg-gray-100 overflow-y-auto max-h-screen">
             <!-- จะถูก JS อัปเดตเนื้อหาให้โดย fetchPendingTransactions() -->
         </div>
 
